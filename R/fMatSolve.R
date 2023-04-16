707c62fe14211f2f5bc4168abf7eeb25b64575bf
psyen0824 <36652557+psyen0824@users.noreply.github.com>
2023-04-15 23:48:31 -0400

    Update fMatSolve.R

--- R/fMatSolve.R
+++ R/fMatSolve.R
@@ -1,13 +1,13 @@
 ## Copyright (C) 2022-2023        Ching-Chuan Chen
 ##
-## This file is part of oneMKLUtil.
+## This file is part of oneMKL.MatrixCal.
 ##
-## oneMKLUtil is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 2 of the License, or
-## (at your option) any later version.
+## oneMKL.MatrixCal is free software: you can redistribute it and/or 
+## modify it under the terms of the GNU General Public License as 
+## published by the Free Software Foundation, either version 2 of 
+## the License, or (at your option) any later version.
 ##
-## oneMKLUtil is distributed in the hope that it will be useful, but
+## oneMKL.MatrixCal is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
