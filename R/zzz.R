7bf26663d00353d4d4bc87cf6fefa283643d3a94
psyen0824 <36652557+psyen0824@users.noreply.github.com>
2023-04-15 23:49:15 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
 ##
 ## This file is part of oneMKL.MatrixCal.
 ##
-## THe R package oneMKL.MatrixCal is free software: you can redistribute 
-## it and/or modify it under the terms of the GNU General Public License 
+## oneMKL.MatrixCal is free software: you can redistribute it and/or 
+## modify it under the terms of the GNU General Public License 
 ## as published by the Free Software Foundation, either version 2 of the 
 ## License, or (at your option) any later version.
 ##
