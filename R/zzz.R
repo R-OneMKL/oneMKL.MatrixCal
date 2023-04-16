1e51e390c19b0cb324a71a602ac26befd8402b13
psyen0824 <36652557+psyen0824@users.noreply.github.com>
2023-04-15 23:39:48 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,19 @@
 ## Copyright (C) 2022-2023        Ching-Chuan Chen
 ##
-## This file is part of oneMKLUtil.
+## This file is part of oneMKL.MatrixCal.
 ##
-## oneMKLUtil is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 2 of the License, or
-## (at your option) any later version.
+## THe R package oneMKL.MatrixCal is free software: you can redistribute 
+## it and/or modify it under the terms of the GNU General Public License 
+## as published by the Free Software Foundation, either version 2 of the 
+## License, or (at your option) any later version.
 ##
-## oneMKLUtil is distributed in the hope that it will be useful, but
+## oneMKL.MatrixCal is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with oneMKLUtil. If not, see <http://www.gnu.org/licenses/>.
+## along with oneMKL.MatrixCal. If not, see <http://www.gnu.org/licenses/>.
 
 #' @importFrom rlang env_unlock env_binding_unlock env_binding_lock env_lock
 
